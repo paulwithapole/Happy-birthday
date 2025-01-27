e744dec8e05473f001fddc4797bb99d1b489f092
paulwithapole <sharanyagopall@outlook.com>
2025-01-27 16:55:47 +0400

    Add files via upload

---
+++ happybirthday.js
@@ -0,0 +1,8 @@
+console.log("yo wsp kash");
+
+document.getElementById("trust").onclick = function () {
+	document.getElementById("skibidi"). textContent = "Happy birthday dude! (So like I'm kinda broke so ts is all you're getting) I hope that you have a great day!!!";
+    window.prompt("Warning: this  website is considered very skibidi, to be safe, enter password error28012011");
+}
+
+
